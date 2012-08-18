8c87241935a93721bf4ec00fbb7634538004e7da
Kieran Kunhya <kierank@ob-encoder.com>
2012-08-19 00:55:09 +0100

    Fix SIMD initalisation problem owing to new merge

--- libavcodec/x86/v210-init.c
+++ libavcodec/x86/v210-init.c
@@ -16,6 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "libavutil/common.h"
 #include "libavutil/cpu.h"
 #include "libavcodec/v210dec.h"
 
